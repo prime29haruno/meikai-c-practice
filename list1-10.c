3b6470afd84566f0b4badb3f0540e3a232404303
prime29haruno <prime29haruno@gmail.com>
2025-08-03 11:27:11 +0900

    Add list1-10.c

---
+++ list1-10.c
@@ -0,0 +1,14 @@
+// 二つの変数を初期化して表示
+
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 57;
+    int y = x + 12;
+
+    printf("xの値は%dです。\n", x);
+    printf("yの値は%dです。\n", y);
+
+    return 0;
+}
