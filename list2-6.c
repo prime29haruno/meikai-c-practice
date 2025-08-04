d9368fad9defbffc370477f22c67271184442e92
prime29haruno <prime29haruno@gmail.com>
2025-08-04 10:53:07 +0900

    Add list2-6.c

---
+++ list2-6.c
@@ -0,0 +1,20 @@
+// 整数と浮動小数点数
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    double x;
+
+    n = 9.99;
+    x = 9.99;
+
+    printf("int型変数 nの値 ：%d\n", n);
+    printf("n / 2 ：%d\n", n / 2);
+        
+    printf("double型変数 xの値 ：%f\n", x);
+    printf("x / 2.0 ：%f\n", x / 2.0);
+
+    return 0;
+}
