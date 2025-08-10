2cbe159b18cc91443adac8a326804c57891b473b
prime29haruno <prime29haruno@gmail.com>
2025-08-10 14:15:02 +0900

    Add list3-14.c

---
+++ list3-14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n1, n2;
+
+    puts("二つの整数を入力せよ。");
+    printf("整数１："); scanf("%d", &n1);
+    printf("整数２："); scanf("%d", &n2);
+
+    int max = n1 > n2 ? n1 : n2;
+
+    printf("大きいほうの値は%dです。\n", max);
+
+    return 0;
+}
