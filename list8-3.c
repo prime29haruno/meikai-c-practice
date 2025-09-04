1ef7b30c5fd3a57dc4244068772013173628d314
prime29haruno <prime29haruno@gmail.com>
2025-09-04 14:45:31 +0900

    Add list8-3.c

---
+++ list8-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define puts_alart(str) { putchar('\a'); puts(str); }
+
+int main(void)
+{
+    int n;
+
+    printf("整数を入力せよ：");
+    scanf("%d", &n);
+
+    if (n)
+        puts_alert("その数はゼロではありません。");
+    else
+        puts_alert("その数はゼロです。");
+
+    return 0;
+}
